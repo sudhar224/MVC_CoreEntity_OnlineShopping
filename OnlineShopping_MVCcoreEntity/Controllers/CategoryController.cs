27f5f9676416fe6b0c952ea5bdece5b16010def9
sudhar224 <sudharsankarthik224@gmail.com>
2024-08-09 19:20:07 +0530

    Initial Commit

---
+++ OnlineShopping_MVCcoreEntity/Controllers/CategoryController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopping.Data;
+using OnlineShopping.Models;
+
+namespace OnlineShopping.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+		public IActionResult Create(Category objCategory)
+		{
+            _db.Categories.Add(objCategory);
+            _db.SaveChanges();
+			return View();
+		}
+
+
+
+	}
+}
